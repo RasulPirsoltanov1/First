2b597a592fac316924182a51d062c01e0daa788a
RasulPirsoltanov1 <7m0uvuf@code.edu.az>
2022-10-27 10:59:54 +0400

    added script js

---
+++ script.js
@@ -0,0 +1,12 @@
+let yas = 17;
+let point = 78;
+
+
+
+console.log((yas > 10 || yas < 20) && point > 60);
+if (((yas > 10 || yas < 20) && point > 60)) {
+    console.log('tebrikler imtahandan ugurla kecdiniz!');
+}
+else {
+    console.log('tebrikler imtahandan ugurla kece bilmediniz!');
+}
